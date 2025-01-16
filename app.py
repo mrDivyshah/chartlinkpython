86f398ac3152a8394e91efe337256e2b5692d847
mrDivyshah <105691678+mrDivyshah@users.noreply.github.com>
2025-01-16 21:10:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/scrape', methods=['GET'])
+def scrape_data():
+    url = 'https://chartink.com/screener/copy-idea-krishchess-combo-mbsanghavi'
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table = soup.find('table', {'id': 'DataTables_Table_0'})
+
+        if table:
+            data = []
+            rows = table.find_all('tr')
+            for row in rows:
+                cells = row.find_all(['td', 'th'])
+                data.append([cell.text.strip() for cell in cells])
+            return jsonify(data)
+        else:
+            return jsonify({'error': 'Table not found'})
+    else:
+        return jsonify({'error': 'Failed to fetch page'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
